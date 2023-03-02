869fe06dd5f9a6a3c29515bc922f76c7551756de
petar.stoyanov <peterstoyanov83@gmail.com>
2023-03-02 13:44:11 +0100

    the output of  "alternative apporach.py" is a csv file created in te same directory.

--- Phone calls cost report/Step 1/alternative approach .py
+++ Phone calls cost report/Step 1/alternative approach .py
@@ -1,4 +1,5 @@
 import mysql.connector
+import pandas as pd
 
 # Connect to the MySQL database
 cnx = mysql.connector.connect(user='walld_mebea', password='nyrVN49VniKJvfU',
@@ -7,19 +8,23 @@ cursor = cnx.cursor()
 
 # Query to get the total duration of phone calls for each department
 query = """
-    SELECT t_employees.name, t_departments.name, SUM(telephone_calls.Gespraechsdauer) as total_duration
-    FROM telephone_calls
-    INNER JOIN employees ON telephone_calls.Rufnummer = employees.id
-    INNER JOIN departments ON employees.DNR = departments.id
-    GROUP BY employees.name, departments.name
+    SELECT t_employees.Name, SUM(telephone_calls.Gespraechsdauer) as total_duration, t_departments.Name as Department, 
+    t_departments.manager
+FROM telephone_calls
+INNER JOIN t_employees ON telephone_calls.Rufnummer = t_employees.phone
+INNER JOIN t_departments ON t_employees.department_id = t_departments.id
+GROUP BY t_employees.Name, Department, t_departments.manager
 """
 
 # Execute the query
 cursor.execute(query)
 
-# Print the results
-for (employee_name, department_name, total_duration, department_manager) in cursor:
-    print(f"{employee_name}: {total_duration} minutes, Department: {department_name} (Manager: {department_manager})")
+# Fetch the results into a pandas dataframe
+results = pd.DataFrame(cursor.fetchall(),
+                       columns=['Employee Name', 'Total Duration (minutes)', 'Department', 'Manager'])
+
+# Export the results to a CSV file and puts it in the same directory with the python code file
+results.to_csv('phone_calls1.csv', index=False)
 
 # Commit the changes to the database
 cnx.commit()
